a7cf0a4645dbb133a7de498da7442b671beca8bc
ebetap <beta.priyoko@students.amikom.ac.id>
2018-10-26 21:46:45 +0700

    add meta tag description

--- application/views/front-end/index.php
+++ application/views/front-end/index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Website Booking Tiket Bis Online"/>
     <title>Sistem Pemesanan Tiket Bus</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
