49461cb203a87785ff2eb261bfa18c4683ed76fb
ebetap <beta.priyoko@students.amikom.ac.id>
2018-10-26 21:53:40 +0700

    merapikan coding index.php

--- application/views/front-end/index.php
+++ application/views/front-end/index.php
@@ -18,11 +18,11 @@
       <div class="container">
         <div class="navbar-header page-scroll">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                      <span class="sr-only">Toggle navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                  </button>
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
           <a class="navbar-brand" href="#">Digital Agency</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse navbar-right navbar-main-collapse">
